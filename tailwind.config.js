e8391e3d8ed9fed71ec1b13260b9d25280da8123
Heshpix <147046534+Heshpix@users.noreply.github.com>
2024-01-10 01:31:03 +0200

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*'],
+  theme: {
+    extend: {
+      colors:{
+        green50:'#9EFF00',
+        green60:'#B1FF33',
+        green70:'#C5FF66',
+        green80:'#D8FF99',
+        green90:'#ECFFCC',
+        green95:'#F5FFE5',
+        green97:'#F9FFF0',
+        green99:'#FDFFFA',
+
+        gray10:'#191919',
+        gray15:'#262626',
+        gray20:'#333333',
+        gray30:'#4C4C4D',
+        gray35:'#59595A',
+        gray40:'#656567',
+        gray60:'#98989A',
+        gray90:'#E6E6E6',
+
+      },
+  backgroundImage:{
+    p1:'url(/**/img/icons/h1bg.jpg);'
+  },
+
+},
+  plugins: [],
+}
+}
